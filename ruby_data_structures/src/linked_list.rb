1dd45840e54f4f79ee6a5cb498902e4ed14c400d
Hack Reactor Students <github@hackreactor.com>
2013-07-16 16:42:32 -0700

    finished ruby LinkedList and specs

--- ruby_data_structures/src/linked_list.rb
+++ ruby_data_structures/src/linked_list.rb
@@ -1,30 +1,59 @@
 class LinkedList
+  attr_accessor :head
+  attr_accessor :tail
   def initialize
     @head = nil
     @tail = nil
   end
-  def head
-    @head
+
+  def addToTail(value)
+    newTail = Node.new(value)
+    oldTail = @tail
+    if @tail
+      @tail.next = newTail
+      @tail = newTail
+      newTail.previous = oldTail
+    else @head = @tail = newTail
+    end
   end
-  def tail
-    @tail
+
+  def addToHead(value)
+    newHead = Node.new(value)
+    oldHead = @head
+    if @head
+      @head.previous = newHead
+      @head = newHead
+      newHead.next = oldHead
+    else @head = @tail = newHead
+    end
   end
-  def add(node)
-    temp = @tail
-    @tail = node
-    node.next = temp
-    if @head == nil
-      @head = @tail
+
+  def contains?(value, node = @head)
+    node.value == value ? true : node.next != nil ? self. contains?(value, node.next) : false
+  end
+
+  def removeHead
+    if @head != nil
+      @head = @head.next
+      @head.previous = nil
+    end
+  end
+
+  def removeTail
+    if @tail != nil
+      @tail = @tail.previous
+      @tail.next = nil
     end
   end
 end
 
 class Node
+  attr_accessor :value
+  attr_accessor :next
+  attr_accessor :previous
   def initialize(value)
     @value = value
     @next = nil
-  end
-  def next
-    @next
+    @previous = nil
   end
 end
