f562ac718372d5b25d2575af2510733a452608c5
Hack Reactor Students <github@hackreactor.com>
2013-07-16 12:08:24 -0700

    included changes in queue_spec

--- ruby_data_structures/src/queue.rb
+++ ruby_data_structures/src/queue.rb
@@ -1,4 +1,19 @@
 class Queue
   def initialize
+    @length = 0
+    @queue = []
+  end
+  def length
+    @length
+  end
+  def add(obj)
+    @queue[@length] = obj
+    @length += 1
+  end
+  def remove
+    unless @length == 0
+      @length -= 1
+      @queue.shift
+    end
   end
 end
